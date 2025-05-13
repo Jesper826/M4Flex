f32f76aaac6eb590b84ae81476da6c3b4e1ac867
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-05-13 12:05:32 +0200

    eerste commit

---
+++ opdrachten/eersteproject/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int leeftijd = 20;
+
+string naam = "Joris";
+
+bool isoud = false;
+
+float prijs = 19.99f;
+
+console.WriteLine("Hallo " + naam + ", je leeftijd is " + leeftijd + " en je bent " + (isoud ? "oud" : "jong") + ".");
