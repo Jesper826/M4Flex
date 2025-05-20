50716ef240cb51d4c416d347444e8348fc90ba57
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-05-20 12:05:14 +0200

    week 2 .2

--- opdrachten/week2api/weerapi/Controllers/WeatherForecastController.cs
+++ opdrachten/week2api/weerapi/Controllers/WeatherForecastController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-
+using weerapi.Models;
 namespace weerapi.Controllers;
 
 [ApiController]
@@ -25,8 +25,10 @@ public class WeatherForecastController : ControllerBase
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+            City = "amsterdam"
         })
         .ToArray();
-    }
+    }    
+
 }
